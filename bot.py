c14899ab0dbfce51211c9e50e30cbe42707b4b4d
Kirill Romakhov <35627288+Romakhov@users.noreply.github.com>
2025-07-08 19:04:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from openai_utils import threadsify_text
 from datetime import datetime
 import sys
 sys.path.append('threads-rag')
-from index import get_rag_results
+from threads-rag.index import get_rag_results
 from telegram.constants import ChatAction
 
 FREE_LIMIT = 5
